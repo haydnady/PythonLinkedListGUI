c12ffcda69f41612e240e505b1769bb3cf140334
Haydn <haydnady@gmail.com>
2020-07-26 15:28:44 -0400

    Find function added and working in main.py

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ class mainApp(Ui_MainWindow):
         userInput = self.inputPlainTextEdit.toPlainText()
 
         # Print list.
+        self.label_2.setText("List Before Operation")
         self.beforeTextEdit.setPlainText(str(self.lList.output_list()))
         #  Add user input to list.
         self.lList.add_list_item(ListNode(userInput))
@@ -39,15 +40,23 @@ class mainApp(Ui_MainWindow):
         userInput = self.inputPlainTextEdit.toPlainText()
 
         # Print list.
+        self.label_2.setText("List Before Operation")
         self.beforeTextEdit.setPlainText(str(self.lList.output_list()))
-        #  Remove user input to list.
+        # Remove user input to list.
         self.lList.remove_list_item_by_value(userInput)
         # Print new/current list.
         self.currentTextEdit.setPlainText(str(self.lList.output_list()))
 
     def findUserInputFromList(self):
-        pass
+        # Get inputPlainTextEdit string
+        userInput = self.inputPlainTextEdit.toPlainText()
 
+        # Print current list.
+        self.currentTextEdit.setPlainText(str(self.lList.output_list()))
+        self.label_2.setText("Element current position")
+        # Print position of item in list.
+        self.beforeTextEdit.setPlainText("Position: " + str(self.lList.unordered_search(userInput)))
+        
 
 if __name__ == "__main__":
     # Create the Qt Application
